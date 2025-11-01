ae65c0c04f3292646105dfefb43ef5af80b4df5a
rajashekar <santhoshreddy.manik@newfold.com>
2025-11-01 14:42:18 +0530

    added libraries for jwt removed deprecated now

--- src/main/java/com/example/restdemo/security/JwtUtil.java
+++ src/main/java/com/example/restdemo/security/JwtUtil.java
@@ -4,16 +4,22 @@ import com.example.restdemo.domain.User;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private static final String jwtSecret = "secret key";
+    // private static final String jwtSecret = "xhs82jskdskncKCJSq8129shcnxz9632";
+    static SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    static String jwtSecret = Base64.getEncoder().encodeToString(secretKey.getEncoded());
     private static final long tokenExpiration = 86400000;
-
+    static SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
 
     public static String generateToken(User user) {
         return Jwts.builder()
@@ -22,13 +28,13 @@ public class JwtUtil {
                 .claim("userId", user.getUserId())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
-                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
 
         } catch (JwtException | IllegalArgumentException e) {
@@ -37,7 +43,7 @@ public class JwtUtil {
     }
 
     public String getUsernameFromToken(String token) {
-        return Jwts.parser().setSigningKey(jwtSecret)
+        return Jwts.parserBuilder().setSigningKey(key).build()
                 .parseClaimsJws(token).getBody().getSubject();
     }
 }
